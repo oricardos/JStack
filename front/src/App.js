23ee7b76286968c235e51ba2d4919e6126b85063
oricardos <oricardo.dev@gmail.com>
2025-01-18 13:05:43 -0300

    state

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Post from "./Post";
 
 function App() {
-  const posts = [
-    { title: "Título do post1", body: "Corpo do post1", likes: 20 },
-    { title: "Título do post2", body: "Corpo do post2", likes: 10 },
-  ];
+  const [posts, setPosts] = useState([
+    { id: Math.random(), title: "Título do post1", body: "Corpo do post1", likes: 20 },
+    { id: Math.random(), title: "Título do post2", body: "Corpo do post2", likes: 10 },
+  ]);
+
+  const handleAddPost = () => {
+    setPosts([
+      ...posts, 
+      {
+        id: Math.random(),
+        title: `Título do post${posts.length + 1}`,
+        body: `Corpo do post${posts.length + 1}`,
+        likes: 0  
+      }
+    ])
+  }
+
+  const handleRemovePost = (id) => {
+    setPosts(posts.filter(post => post.id !== id));
+  }
 
   return (
     <>
       <Header title="JStack's Blog">
-        <h2>Posts da semana</h2>
+        <h2>Posts da semana
+          <button onClick={handleAddPost}>Adicionar Post</button>
+        </h2>
       </Header>
       {posts.map((post, index) => (
-        <Post key={index} post={post} likes={post.likes} />
+        <>
+          <Post key={post.id} post={post} likes={post.likes} />
+          <button onClick={()=> handleRemovePost(post.id)}>Remover post {index + 1}</button>
+        </>
       ))}
     </>
   );
