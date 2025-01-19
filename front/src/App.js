9205afa2a517a3ba91125c912c7d846532455895
oricardos <oricardo.dev@gmail.com>
2025-01-19 18:07:54 -0300

    remove item

--- front/src/App.js
+++ front/src/App.js
@@ -4,37 +4,52 @@ import Post from "./Post";
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: Math.random(), title: "Título do post1", body: "Corpo do post1", likes: 20 },
-    { id: Math.random(), title: "Título do post2", body: "Corpo do post2", likes: 10 },
+    {
+      id: Math.random(),
+      title: "Título do post1",
+      body: "Corpo do post1",
+      likes: 20,
+    },
+    {
+      id: Math.random(),
+      title: "Título do post2",
+      body: "Corpo do post2",
+      likes: 10,
+    },
   ]);
 
   const handleAddPost = () => {
     setPosts([
-      ...posts, 
+      ...posts,
       {
         id: Math.random(),
         title: `Título do post${posts.length + 1}`,
         body: `Corpo do post${posts.length + 1}`,
-        likes: 0  
-      }
-    ])
-  }
+        likes: 0,
+      },
+    ]);
+  };
 
-  const handleRemovePost = (id) => {
-    setPosts(posts.filter(post => post.id !== id));
-  }
+  const handleRemovePost = (postId) => {
+    setPosts((prevState) => prevState.filter((post) => post.id !== postId));
+  };
 
   return (
     <>
       <Header title="JStack's Blog">
-        <h2>Posts da semana
+        <h2>
+          Posts da semana
           <button onClick={handleAddPost}>Adicionar Post</button>
         </h2>
       </Header>
-      {posts.map((post, index) => (
+      {posts.map((post) => (
         <>
-          <Post key={post.id} post={post} likes={post.likes} />
-          <button onClick={()=> handleRemovePost(post.id)}>Remover post {index + 1}</button>
+          <Post
+            key={post.id}
+            post={post}
+            likes={post.likes}
+            onRemove={handleRemovePost}
+          />
         </>
       ))}
     </>
--- front/src/Post.js
+++ front/src/Post.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default function Post({ post, likes }) {
+export default function Post({ post, likes, onRemove }) {
   return (
     <>
       <article>
         <strong>{post.title}</strong>
+        <button onClick={() => onRemove(post.id)}>Remover post</button>
         <br />
         <small>{post.body}</small>
         <br />
@@ -18,8 +19,10 @@ export default function Post({ post, likes }) {
 
 Post.propTypes = {
   post: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     body: PropTypes.string.isRequired,
   }).isRequired,
   likes: PropTypes.number.isRequired,
+  onRemove: PropTypes.func.isRequired,
 };
