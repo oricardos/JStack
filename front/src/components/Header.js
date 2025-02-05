2ed4ee747c09763fa66aca2ecec11d7f7f9495cc
oricardos <oricardo.dev@gmail.com>
2025-02-04 23:20:22 -0300

    css-in-js

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -2,12 +2,17 @@ import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import Button from "./Button";
 import { ThemeContext } from "../context/ThemeContext";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: #637BF3
+`;
 
 export default function Header({ title, children }) {
   const { onToggleTheme } = useContext(ThemeContext);
   return (
     <header>
-      <h1>{title}</h1>
+      <Title>{title}</Title>
       <Button onClick={onToggleTheme}>Mudar Tema</Button>
       {children}
       <hr />
