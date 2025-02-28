5b0e400d125df07f357b907e2527f374ed6e58f1
oricardos <oricardo.dev@gmail.com>
2025-02-28 14:37:45 -0300

    feat: refactor App component to manage state and lifecycle methods, replacing ThemeConsumer with direct context usage

--- front/src/App.js
+++ front/src/App.js
@@ -6,33 +6,56 @@ import Layout from "./components/Layout";
 import { TemaProvider, ThemeContext } from "./context/ThemeContext";
 
 class App extends React.Component {
+  state = {
+    changed: false,
+  };
+
+  componentDidMount() {
+    console.log("componentDidMount executed"); // equivalente à useEffect(() => {}, []) executa uma vez, quando o componente é montado
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log({
+      currentState: this.state,
+      prevProps,
+      prevState,
+    });
+  }
+
+  componentDidCatch(error, info) {
+    console.log({ error, info });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log({
+      currentState: this.state,
+      nextProps,
+      nextState,
+    });
+
+    return true;
+  }
+
+  componentWillUnmount() {}
+
   render() {
+    console.log("rendered"); // equivalente à useEffect(() =>{}) executa sempre que o componente é renderizado
     return (
       <TemaProvider>
-        <ThemeConsumer />
+        <button onClick={() => this.setState({ changed: true })}>
+          Change State
+        </button>
+        <ThemeContext.Consumer>
+          {(contextData) => (
+            <ThemeProvider theme={contextData.currentTheme}>
+              <GlobalStyle />
+              <Layout />
+            </ThemeProvider>
+          )}
+        </ThemeContext.Consumer>
       </TemaProvider>
     );
   }
-}
-
-function ThemeConsumer() {
-  const { currentTheme } = useContext(ThemeContext);
-
-  const firstRender = useRef(true);
-
-  //evitando primeiro render
-  useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false;
-    }
-  }, [currentTheme]);
-
-  return (
-    <ThemeProvider theme={currentTheme}>
-      <GlobalStyle />
-      <Layout />
-    </ThemeProvider>
-  );
 }
 
 export default App;
